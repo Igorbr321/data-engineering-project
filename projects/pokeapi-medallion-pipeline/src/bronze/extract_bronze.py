7f210b17b2915944e257bd686f1aa5ee5cc8993c
Igorbr321 <igormatheus010514@outlook.com>
2026-03-26 12:08:15 -0300

    refactor: corrigindo loggings e organizando função de extração dos dados da API

--- projects/pokeapi-medallion-pipeline/src/bronze/extract_bronze.py
+++ projects/pokeapi-medallion-pipeline/src/bronze/extract_bronze.py
@@ -2,29 +2,31 @@ import requests
 import logging
 import pandas as pd
 
+BRONZE_PREFIX = "🟤 [BRONZE] -"
+
 API_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=1000"
 API_POKEMON_UNITARIO = "https://pokeapi.co/api/v2/pokemon/{}"
 
 
-def extract_pokemons_bronze(csv_path: str = "endpoints.csv"):
-    logging.info("Iniciando extração dos endpoints da PokeAPI")
+def extract_pokemons_bronze() -> list[dict]:
+    logging.info(f"{BRONZE_PREFIX} Iniciando extração dos endpoints da PokeAPI")
 
     response = requests.get(API_POKEMON)
     df = pd.DataFrame(response.json()["results"])
 
     detalhes = []
 
-    for name in df["name"].head(100):
+    for name in df["name"]:
         url = API_POKEMON_UNITARIO.format(name)
         response = requests.get(url)
         detalhes.append(response.json())
 
-    df_detalhes = pd.DataFrame(detalhes)
-    df_detalhes.to_csv(f'Data\{csv_path}', index=False)
+    #df_detalhes = pd.DataFrame(detalhes)
+    #df_detalhes.to_csv(f'Data\\extract_pokemon_bronze.csv', index=False)
 
-    logging.info("Extração dos endpoints concluída e salva em CSV")
+    logging.info(f"{BRONZE_PREFIX} Extração dos endpoints da PokeAPI concluída.")
     
-    return df_detalhes
+    return detalhes
 
 
 
--- projects/pokeapi-medallion-pipeline/src/bronze/main.py
+++ projects/pokeapi-medallion-pipeline/src/bronze/main.py
@@ -2,24 +2,31 @@ import logging
 import time
 
 from src.bronze.extract_bronze import extract_pokemons_bronze
+from src.bronze.insert_bronze_s3 import insert_bronze_s3
 from src.utils.utils import init_logging, execution_time
 
+BRONZE_PREFIX = "🟤 [BRONZE] -"
+
 def main():
-    init_logging('bronze')
+    init_logging("bronze")
 
     error = "N"
     start_time = time.time()
 
     try:
-        df = extract_pokemons_bronze()
+        list_detalhes = extract_pokemons_bronze()
+        logging.info(f"{BRONZE_PREFIX} Registros extraídos: {len(list_detalhes)}")
+
+        insert_bronze_s3(list_detalhes)
 
     except Exception as e:
-        logging.error(f"Erro durante a execução: {e}")
-        error = "S"
+        logging.error(f"❌Erro durante a execução: {e}")
+        raise
 
     finally:
         exec_time = execution_time(start_time)
-        logging.info(f"Fim do processo. Tempo de execução: {exec_time} minutos.")
+        logging.info(f"✅ Fim do processo. Tempo de execução: {exec_time} minutos.")
+
 
 if __name__ == "__main__":
     main()
